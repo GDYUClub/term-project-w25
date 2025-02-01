97919583750b80ac0b0b1fc1f8e2c0c2b54aabd0
JUB_Yoush <yoush1337@gmail.com>
2025-02-01 02:24:48 -0500

    clue datatype

---
+++ src/clues/clue.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Clue
+
+@export var id:String 
+@export var name:String 
+@export var desc:String 
+@export var icon:Texture
+@export var panel:Texture
+@export var environment_sprite:Texture
+@export var picks_up:bool
--- src/player/code/player.gd
+++ src/player/code/player.gd
@@ -14,11 +14,11 @@ var current_sprint_speed: float = SPRINT_SPEED
 
 var move = Global.direction
 
-enum MoveType {
-	TOP_DOWN = 0,
-	SIDE_SCROLLER = 1
+enum MOVETYPES {
+	TOP_DOWN,
+	SIDE_SCROLLER,
 }
-
+var current_move_type:MOVETYPES
 
 var on_ladder: bool = false
 
@@ -33,14 +33,14 @@ var crouch_cshape = preload("res://assets/resources/crouch_cshape.tres")
 # Top down movement function
 func _top_down():
 	
-	
 	var direction_vector := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	
 	current_speed = CROUCH_SPEED if is_crouching else WALK_SPEED
 	current_sprint_speed = SPRINT_SPEED if not is_crouching else WALK_SPEED
 	
-	velocity.x = direction_vector.x * (current_sprint_speed if Input.is_action_pressed("sprint") else current_speed) if direction_vector.x else move_toward(velocity.x, 0, current_speed)
-	velocity.y = direction_vector.y * current_speed if direction_vector.y else move_toward(velocity.y, 0, current_speed)
+	# velocity.x = direction_vector.x * (current_sprint_speed if Input.is_action_pressed("sprint") else current_speed) if direction_vector.x else move_toward(velocity.x, 0, current_speed)
+	# velocity.y = direction_vector.y * current_speed if direction_vector.y else move_toward(velocity.y, 0, current_speed)
+	velocity = current_speed * direction_vector
 
 
 # Side scroller movement function
@@ -68,20 +68,28 @@ func _climb():
 
 
 # Crouch function
-func _crouch():
-	is_crouching = true if !is_crouching else false
+func _toggle_crouch():
+	is_crouching = !is_crouching
+	#is_crouching = true if !is_crouching else false
 	cshape.shape = crouch_cshape if is_crouching else stand_cshape
 
 
 func _physics_process(delta: float) -> void:
+	
+	
+
 	move = Global.direction
+	
 	if Input.is_action_just_pressed("crouch"):
-		_crouch()
-	if MoveType.find_key(move) == "TOP_DOWN":
+		_toggle_crouch()
+	
+	if is_crouching:
+		current_speed = CROUCH_SPEED
+		current_speed = CROUCH_SPEED
+
+	if current_move_type == MOVETYPES.TOP_DOWN: 
 		_top_down()
 	else:
 		_side_scroller(delta)
-
-	print(MoveType.find_key(move))
-	
+	#print(MoveType.find_key(move))
 	move_and_slide()
