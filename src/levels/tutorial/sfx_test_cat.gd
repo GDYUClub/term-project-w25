513eb6497c5b51c2b5849c4f613738314b9fa393
JUB_Yoush <yoush1337@gmail.com>
2025-03-06 11:08:56 -0500

    rm test files

--- src/levels/tutorial/sfx_test_cat.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _ready() -> void:
-	area_entered.connect(
-	func(_area:Area2D) -> void:
-		AudioManager.play_sfx(preload("res://assets/sound/sfx/lighter_sfx.ogg"))	
-	)
-	pass
