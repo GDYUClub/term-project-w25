20fd7bfae8def06187f05b9dcabe20a4ff4af39b
JUB_Yoush <yoush1337@gmail.com>
2025-03-20 11:26:42 -0400

    level 2-1 stuff ported back

--- src/levels/2/level_2_1.gd
+++ src/levels/2/level_2_1.gd
@@ -1,25 +1,5 @@
 extends GameplayPage
 
-const PANEL_CENTERS = [
-Vector2i(890,180),
-Vector2i(1560,180),
-Vector2i(867,726),
-Vector2i(1305,510),
-Vector2i(1717,510),
-Vector2i(1560,878),
-]
-
-var overlapping_panel_name:String = ""
-
 func _ready() -> void:
-	for triggerArea:Area2D in $PanelTriggers.get_children():
-		triggerArea.area_entered.connect(func(trigger): overlapping_panel_name = triggerArea.name)
-		triggerArea.area_exited.connect(func(trigger): overlapping_panel_name = "")
-
-func _toggle_panel(panel_number:String) -> void:
-	if panel_number == "":
-		return
-	var panelCover:Polygon2D = $PanelCovers.get_node(panel_number)
-	$Cursor.position = PANEL_CENTERS[int(panel_number)]
-	$Cursor.visible = !$Cursor.visible
-	panelCover.visible = !panelCover.visible 
+	super._ready()
+	point_click = true
