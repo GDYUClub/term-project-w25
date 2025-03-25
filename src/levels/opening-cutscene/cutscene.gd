c996f0ce3dd669273c90d448816e43206dc4184e
JUB_Yoush <yoush1337@gmail.com>
2025-03-25 15:17:17 -0400

    okay we'll just do it manually

--- src/levels/opening-cutscene/cutscene.gd
+++ src/levels/opening-cutscene/cutscene.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-var current_page:int = 1
-var current_panel :int= 1
+var current_page:int = 0
+var current_panel :int= 0
 
 @onready var camera:Camera2D = $Camera2D
 const DEFAULT_SPEED = .2
 const TWEEN_SPEED = .2
+const PAGES = 1
 # panels are 1 indexed so the first thing dosen't matter
 # [x,y,z] x = color of text (0= white 1= black) y = delay until next panel, scroll_speed
 const PANEL_INFO: = [
@@ -33,26 +34,28 @@ signal finished_scrolling
 signal finished_rendering
 
 func render_panel_bundle() -> void:
-	var panel :Sprite2D= get_node("%d/Panels/%d" % [current_page, current_panel])
-	var bubble :Sprite2D= get_node("%d/Bubbles/B%d" % [current_page, current_panel])
-	var text :Label= get_node("%d/Text/L%d" % [current_page, current_panel])
-	if panel == null:
-		printerr("panel dosen't exist what are you even doing")
-	panel.modulate = Color(1,1,1,0)
-	var tween:Tween = get_tree().create_tween()
-
-	if panel != null:
-		tween.tween_property(panel,"modulate",Color(Color.WHITE),TWEEN_SPEED)
+	var panel :Node2D= get_node("%d/%d/Panel" % [current_page, current_panel])
+	var bubbleNode :Node2D= get_node("%d/%d/Bubbles" % [current_page, current_panel])
+	var labelNode :Node2D= get_node("%d/%d/Text" % [current_page, current_panel])
 	
-	if bubble != null:
-		tween.parallel().tween_property(bubble,"modulate",Color(Color.WHITE),TWEEN_SPEED)
+	var bubbles :Array[Node]= bubbleNode.get_children()
+	var labels:Array[Node]= labelNode.get_children()
+	var tween:Tween = get_tree().create_tween()
+	
+	tween.tween_property(panel,"modulate",Color(Color.WHITE),TWEEN_SPEED)
+	
+	await tween.finished
 
-	if text != null:
+	while !bubbles.is_empty():
+		var bubble :Sprite2D = bubbles.pop_front()
+		var text :Label = labels.pop_front()
+		tween.tween_property(bubble,"modulate",Color(Color.WHITE),TWEEN_SPEED)
+
 		var text_color:Color = COLOR_MAP[PANEL_INFO[current_panel][0]]
 		tween.parallel().tween_property(text,"modulate",Color(text_color),TWEEN_SPEED)
 
-	await tween.finished
-	if bubble
+		await tween.finished
+
 	
 
 func _ready() -> void:
@@ -79,10 +82,22 @@ func _process(delta: float) -> void:
 
 func hide_everything() -> void:
 	print(PANEL_INFO.size())
-	for node in get_node("%d/Panels" % current_page).get_children():	
-		node.modulate = Color(1,1,1,0)
-	for node in get_node("%d/Bubbles" % current_page).get_children():	
-		node.modulate = Color(1,1,1,0)
-	for node in get_node("%d/Text" % current_page).get_children():	
-		node.modulate = COLOR_MAP[PANEL_INFO[current_panel][0]]
-		node.modulate.a = 0
+	for i in PAGES:
+		var page = get_node("%d" % i)
+		for panel_bundle in page.get_children():	
+			var panel :Sprite2D= panel_bundle.get_node("Panel")
+			var bubbleNode :Node2D= get_node("%d/%d/Bubbles" % [current_page, current_panel])
+			var labelNode :Node2D= get_node("%d/%d/Text" % [current_page, current_panel])
+			panel.modulate = Color(1,1,1,0)
+			for bubble in bubbleNode.get_children():
+				bubble.modulate = Color(1,1,1,0)
+			for text in labelNode.get_children():
+				text.modulate = Color(1,1,1,0)
+
+	# for node in get_node("%d/Panels" % current_page).get_children():	
+	# 	node.modulate = Color(1,1,1,0)
+	# for node in get_node("%d/Bubbles" % current_page).get_children():	
+	# 	node.modulate = Color(1,1,1,0)
+	# for node in get_node("%d/Text" % current_page).get_children():	
+	# 	node.modulate = COLOR_MAP[PANEL_INFO[current_panel][0]]
+	# 	node.modulate.a = 0
