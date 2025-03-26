824b8275e15af7bc0e7512997e12e1f7ad1bc690
JUB_Yoush <yoush1337@gmail.com>
2025-03-26 11:49:07 -0400

    33/5 pages done

--- src/levels/opening-cutscene/cutscene_1.gd
+++ src/levels/opening-cutscene/cutscene_1.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
-const PAGES = 1
+const ANIMATIONS = ["page1","page2","page3","page4","page5"] 
+#const ANIMATIONS = ["page3","page2"] 
+@onready var animPlayer :AnimationPlayer = $AnimationPlayer
+@onready var ref :Sprite2D= $reference
+
 func _ready() -> void:
+	hide_everything()
+	for i in range(5):
+		var panel :Node2D = get_node("%d" % i)
+		panel.visible = true
+		animPlayer.play(ANIMATIONS[i])
+		await animPlayer.animation_finished
+		panel.visible = false
+
 	pass
 
-func hide_everything() -> void:
-	for i in PAGES:
-		var page = get_node("%d" % i)
-		for panel_bundle in page.get_children():	
-			var panel :Sprite2D= panel_bundle.get_node("Panel")
-			var bubbleNode :Node2D= panel_bundle.get_node("Bubbles")
-			var labelNode :Node2D= get_node("%d/%d/Text" )
-			panel.modulate = Color(1,1,1,0)
-			for bubble in bubbleNode.get_children():
-				bubble.modulate = Color(1,1,1,0)
-			for text in labelNode.get_children():
-				text.modulate = Color(1,1,1,0)
+func hide_everything():
+	ref.visible = false
+	for i in range(5):
+		var panel :Node2D = get_node("%d" % i)
+		panel.visible = false
